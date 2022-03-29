2f7cce1200089698cb6b70dff0a7fd98d888a61c
nehanisarkhan <nehaen1036@gmail.com>
2022-03-29 18:52:35 +0530

    2 files modified, 3 added

---
+++ src/AccountMembers.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AccountMembers() {
+  return <h1>Hello World</h1>;
+}
+
+export default AccountMembers;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import { data } from './Data.js';
-import React, { useState } from 'react';
+import AccountMembers from './AccountMembers';
+import JobSettings from './JobSettings';
+import CompanySettings from './CompanySettings';
+
 function Sidebar() {
   const [copied, setCopied] = useState(false);
+  const [selectedTab, setSelectedTab] = useState(0);
+
   function copy() {
     const el = document.createElement('input');
     el.value = window.location.href;
@@ -13,25 +18,78 @@ function Sidebar() {
     document.body.removeChild(el);
     setCopied(true);
   }
+
+  const tabItems = [
+    {
+      label: 'Account Members',
+      value: 0,
+    },
+    {
+      label: 'Job Settings',
+      value: 1,
+    },
+    {
+      label: 'Company Settings',
+      value: 2,
+    },
+    {
+      label: 'Student Settings',
+      value: 3,
+    },
+    {
+      label: 'Event Settings',
+      value: 4,
+    },
+    {
+      label: 'Subscription & Payments',
+      value: 5,
+    },
+    {
+      label: 'Integrations',
+      value: 6,
+    },
+  ];
+  const onTabChange = (item) => {
+    console.log('clicked');
+    setSelectedTab(item);
+  };
+  const renderTemplateContainer = () => {
+    switch (selectedTab) {
+      case 0:
+        return <AccountMembers />;
+
+      case 1:
+        return <JobSettings />;
+
+      case 2:
+        return <CompanySettings />;
+      case 3:
+        return <h1>Have fun </h1>;
+    }
+  };
+
   return (
     <>
       <div className="Sidebar">
         <ul className="sidebarList">
-          {data.map((item, key) => {
+          {tabItems.map((item, key) => {
             return (
               <li
-                key={key}
+                key={item.value}
                 className="row"
-                id={window.location.pathname == item.link ? 'active' : ''}
-                onClick={() => (window.location.pathname = item.link)}
+                // className={
+                //   window.location.pathname == item.link ? 'active' : 'row'
+                // }
+                onClick={() => onTabChange(item.value)}
               >
-                {item.name}
+                {item.label}
               </li>
             );
           })}
         </ul>
       </div>
-      <button onClick={copy}>{!copied ? 'Copy link' : 'Copied!'}</button>
+      {/* <button onClick={copy}>{!copied ? 'Copy link' : 'Copied!'}</button> */}
+      <div>{renderTemplateContainer()}</div>
     </>
   );
 }
