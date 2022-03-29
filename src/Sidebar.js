32b58cefdd629a6d9939579183c85720c8c3567b
nehanisarkhan <nehaen1036@gmail.com>
2022-03-29 15:46:16 +0530

    a copy link button added

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import './style.css';
 import { data } from './Data.js';
+import React, { useState } from 'react';
 function Sidebar() {
+  const [copied, setCopied] = useState(false);
+  function copy() {
+    const el = document.createElement('input');
+    el.value = window.location.href;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    setCopied(true);
+  }
   return (
     <div className="Sidebar">
       <ul className="sidebarList">
@@ -18,6 +29,7 @@ function Sidebar() {
           );
         })}
       </ul>
+      <button onClick={copy}>{!copied ? 'Copy link' : 'Copied!'}</button>
     </div>
   );
 }
